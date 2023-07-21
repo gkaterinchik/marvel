020e2042842487b4198e8f06daf49fd3e28fc287
gkaterinchik <ggg140@yandex.ru>
2023-07-21 11:09:01 +0300

    some changes

--- src/main/java/com/stm/marvel/Controllers/FileUploadController.java
+++ src/main/java/com/stm/marvel/Controllers/FileUploadController.java
@@ -1,22 +1,15 @@
 package com.stm.marvel.Controllers;
 
-import java.io.IOException;
-import java.util.stream.Collectors;
-
+import com.stm.marvel.Services.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.stm.marvel.Exceptions.StorageFileNotFoundException;
-import com.stm.marvel.Services.StorageService;
 
 @Controller
 @RequestMapping("/api/v1/public")
@@ -41,13 +34,9 @@ public class FileUploadController {
 
     @PostMapping(value = "/files",consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
     @ResponseBody
-    public String handleFileUpload(@RequestPart("file") MultipartFile file,
-                                   RedirectAttributes redirectAttributes) {
+    public String handleFileUpload(@RequestPart("file") MultipartFile file) {
         System.out.println(file.isEmpty());
         storageService.store(file);
-        redirectAttributes.addFlashAttribute("message",
-                "You successfully uploaded " + file.getOriginalFilename() + "!");
-
         return file.getOriginalFilename();
     }
 
