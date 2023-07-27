0bd0f5af88827e4a4170ef3a70d83af83bac2b46
gkaterinchik <ggg140@yandex.ru>
2023-07-27 12:29:42 +0300

    resolve Hibernate n+1 problem

--- src/main/java/com/stm/marvel/Repositories/CharacterRepository.java
+++ src/main/java/com/stm/marvel/Repositories/CharacterRepository.java
@@ -1,6 +1,11 @@
 package com.stm.marvel.Repositories;
 
 import com.stm.marvel.Entities.Character;
+import com.stm.marvel.Entities.Comics;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
@@ -10,6 +15,14 @@ import java.util.Optional;
 @Repository
 public interface CharacterRepository extends JpaRepository<Character,Integer>, JpaSpecificationExecutor<Character> {
 Optional<Character> findByName(String name);
+    @EntityGraph(
+            type = EntityGraph.EntityGraphType.FETCH,
+            attributePaths = {
+                    "comics"
+            }
+
+    )
+    Page<Character> findAll(Specification<Character> spec, Pageable pageable);
 
 
 
--- src/main/java/com/stm/marvel/Repositories/ComicsRepository.java
+++ src/main/java/com/stm/marvel/Repositories/ComicsRepository.java
@@ -1,15 +1,27 @@
 package com.stm.marvel.Repositories;
 
-import com.stm.marvel.Entities.Character;
 import com.stm.marvel.Entities.Comics;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ComicsRepository extends JpaRepository<Comics, Integer>, JpaSpecificationExecutor<Comics> {
+    @EntityGraph(
+            type = EntityGraph.EntityGraphType.FETCH,
+            attributePaths = {
+                    "characters"
+            }
+
+    )
+    Page<Comics> findAll(Specification<Comics> spec, Pageable pageable);
     Optional<Comics> findByTitle(String title);
 
 }
